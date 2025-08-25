3cde42fe073da07d176daf8eb1caaba87b832c20
xssdsanda-sudo <xssdsanda@gmail.com>
2025-08-25 21:37:46 +0500

    Add .next/static/chunks/src_app_sync_page_tsx_a993527f._.js 1756139866078

---
+++ .next/static/chunks/src_app_sync_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f569790d._.js",
+  "static/chunks/node_modules_f5375be5._.js"
+],
+    source: "dynamic"
+});
